2a0ccfa001b0773306ef7238259b5f702fef7b55
John Doe <johndoe@example.com>
2024-03-16 11:07:16 +0800

    Add project files.

---
+++ PCCO104-VisualStudio-Tiña/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace PCCO104_VisualStudio_Tiña
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
+
+            string[] names = { "Name1", "Name2" };
+            foreach (var name in names)
+            {
+                Console.WriteLine($"{name}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
